002c8f86d6d9078e515057ea297086edb3cbd4a9
Arthur Araujo <arthur.araujo@tutanota.com>
2023-02-13 12:08:40 -0300

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,50 @@
 export default {
-	'env': {
-		'browser': true,
-		'es2021': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended',
-		"prettier",
-        "prettier/react",
-		'airbnb',
-        'airbnb/hooks'
-	],
-	'overrides': [
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'react',
-		'react-native',
-		'@typescript-eslint'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			2
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/react',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'object', 'type'],
+        alphabetize: { order: 'asc' },
+      },
+    ],
+    'react-hooks/exhaustive-deps': 'error',
+  },
 };
--- util/colors.ts
+++ util/colors.ts
@@ -1,30 +1,31 @@
 const Colors = {
   white: {
     regular: '#f2f2f2',
-    darker:  '#b5b5b5',
+    darker: '#b5b5b5',
   },
   black: {
     almostGrey: '#4d4b4b',
-    lightest:   '#3f3e3e',
-    light:      '#2b2b2b',
-    regular:    '#303030',
-    darker:     '#1c1c1c',
-    darkest:    '#121212',
+    lightest: '#3f3e3e',
+    light: '#2b2b2b',
+    regular: '#303030',
+    darker: '#1c1c1c',
+    darkest: '#121212',
   },
-  accent: { // red
-    light:   '#ff361e',
+  accent: {
+    // red
+    light: '#ff361e',
     regular: '#e1321a',
   },
   green: {
-    light:   '#7bc91f',
+    light: '#7bc91f',
     regular: '#6ab017',
   },
   yellow: {
-    light:   '#ffd00a',
-    regular: '#ffc005'
+    light: '#ffd00a',
+    regular: '#ffc005',
   },
   blue: {
-    light:   '#0071ff',
+    light: '#0071ff',
     regular: '#004f9e',
   },
 };
